04d77fff2ef0a7fb07864e5a5a1a742642c9fac8
Maxime RODRIGUEZ <max.rodriguezidf@gmail.com>
2023-02-02 20:11:08 +0100

    first commit

---
+++ src/main/java/com/example/testlast_projo/TestlastProjoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testlast_projo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlastProjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestlastProjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testlast_projo/TestlastProjoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testlast_projo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlastProjoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
